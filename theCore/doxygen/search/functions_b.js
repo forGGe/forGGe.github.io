7596f35240849004067e8cd2279df0f542dc7fa2
Travis CI <travis@travis-ci.org>
2018-10-23 12:54:07 +0000

    Travis update from 776

--- theCore/doxygen/search/functions_b.js
+++ theCore/doxygen/search/functions_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['kernel_5fmain',['kernel_main',['../sys_8cpp.html#ada8402e0c504af8cafef5cc76c076003',1,'kernel_main():&#160;kernel_main.c'],['../kernel__main_8c.html#ada8402e0c504af8cafef5cc76c076003',1,'kernel_main():&#160;kernel_main.c'],['../kernel__stubs_8c.html#ada8402e0c504af8cafef5cc76c076003',1,'kernel_main():&#160;kernel_stubs.c']]]
+  ['kernel_5fmain',['kernel_main',['../kernel__main_8c.html#ada8402e0c504af8cafef5cc76c076003',1,'kernel_main():&#160;kernel_main.c'],['../kernel__stubs_8c.html#ada8402e0c504af8cafef5cc76c076003',1,'kernel_main():&#160;kernel_stubs.c'],['../sys_8cpp.html#ada8402e0c504af8cafef5cc76c076003',1,'kernel_main():&#160;kernel_main.c']]]
 ];
